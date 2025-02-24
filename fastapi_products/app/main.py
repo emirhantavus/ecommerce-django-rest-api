5d0dba7445ff94d691cc24a1e6f14c595454f88e
Emirhan Tavus <emirhantavus17@gmail.com>
2025-02-24 23:47:11 +0300

    Monolithic architecture is better for now fastapi products deleted

--- fastapi_products/app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.api.v1.endpoints.products import router as products_router
-
-app = FastAPI(title="Products Service")
-
-app.include_router(products_router, prefix="/products", tags=["products"])
