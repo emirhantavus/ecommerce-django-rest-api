29c8d9d4e48b2ed52e8529ab3d543dc37d3c0bed
Emirhan Tavus <emirhantavus17@gmail.com>
2025-02-28 04:55:31 +0300

    product serializer methods add

--- products/serializers.py
+++ products/serializers.py
@@ -6,12 +6,32 @@ from .models import Product, Category
 User = get_user_model()
 
 class CategorySerializer(serializers.ModelSerializer):
+      subcategories = serializers.SerializerMethodField()
       class Meta:
             model = Category
-            fields = '__all__'
+            fields = ('id','name','level','subcategories')
+            
+      def get_subcategories(self,obj):
+            return CategorySerializer(obj.subcategories.all(), many=True).data
             
             
 class ProductSerializer(serializers.ModelSerializer):
+      seller_products = serializers.SerializerMethodField()
+      discounted_price = serializers.SerializerMethodField()
+      low_stock = serializers.SerializerMethodField()
       class Meta:
             model = Product
-            fields = '__all__'
+            fields = ('id','name','price','stock','active','seller_products','discounted_price','low_stock')
+            
+      def get_seller_products(self, obj):
+            return ProductSerializer(Product.objects.filter(seller=obj.seller),many=True).data
+      
+      def get_discounted_price(self,obj):
+            if obj.discount and obj.discount_rate > 0:
+                  return round(obj.price * (1 - obj.discount_rate / 100), 2)
+            return obj.price
+            
+      def get_low_stock(self, obj):
+            if obj.stock <= 5:
+                  return {"low_stock":True}
+            return {"low_stock":False}
