2a6076f7a532f2137b88b2fa1609060a8e0a9152
Emirhan Tavus <emirhantavus17@gmail.com>
2025-02-28 04:24:10 +0300

    add level for category and discount for products.

--- products/models.py
+++ products/models.py
@@ -8,9 +8,17 @@ class Category(models.Model):
       parent = models.ForeignKey(
             "self", on_delete=models.CASCADE, null=True,blank=True,related_name='subcategories'
       )
+      level = models.IntegerField(default=0)
       
       class Meta:
             verbose_name_plural = "Categories"
+            
+      def save(self, *args, **kwargs):
+            if self.parent:
+                  self.level = self.parent.level + 1
+            else:
+                  self.level = 0
+            super().save(*args,**kwargs)
       
       def __str__(self):
             return f"Category name: {self.name}"
@@ -20,16 +28,29 @@ class Product(models.Model):
       name = models.CharField(max_length=255)
       description = models.TextField()
       price = models.DecimalField(max_digits=10, decimal_places=2)
-      stock = models.PositiveIntegerField(default=0)
+      stock = models.PositiveIntegerField(null=False)
       category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True)
       seller = models.ForeignKey(user,on_delete=models.CASCADE)
       image = models.ImageField(upload_to='products/')
       created_at = models.DateTimeField(auto_now_add=True)
-      updated_at = models.DateTimeField(auto_now_add=True)
+      updated_at = models.DateTimeField(auto_now=True)
       active = models.BooleanField(default=True)
+      discount = models.BooleanField(default=False)
+      discount_rate = models.IntegerField(default=0)
       
       class Meta:
             ordering = ["-created_at"]
+            
+      def save(self, *args, **kwargs):
+            if self.stock == 0:
+                  self.active = False
+            super().save(*args,**kwargs)
+      
+      @property
+      def discount_price(self):
+            if self.discount:
+                  return self.price * (1 - self.discount_rate/100) #discounted price
+            return self.price # normal price If there is not discount
             
       def __str__(self):
             return f"Product name: {self.name}"
